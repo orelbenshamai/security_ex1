296ddc53741209b27702ebc309a57afb8a9cd5d4
orelbenshamai <79166869+orelbenshamai@users.noreply.github.com>
2023-05-24 15:47:34 +0300

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -24,13 +24,13 @@ def decrypt(ciphertext, key, iv):
     block_size = DES.block_size
     cipher_decrypt = DES.new(key, DES.MODE_CBC, iv)
     decrypted_padded_plaintext = cipher_decrypt.decrypt(ciphertext)
-    decrypted_unpadded_plaintext = unpad(decrypted_padded_plaintext, block_size)
-    return decrypted_unpadded_plaintext
+    decrypted_un_padded_plaintext = unpad(decrypted_padded_plaintext, block_size)
+    return decrypted_un_padded_plaintext
 
 
 def increment_ith_byte(i, block):
     byte_array = bytearray(block)
-    byte_array[i] += 1
+    byte_array[i] = (byte_array[i] + 1) % 256
     return byte_array
 
 
@@ -38,31 +38,40 @@ def extract_plain_byte(pad_value, cipher_value, changed_value):
     return xor(pad_value, cipher_value, changed_value)
 
 
-def extract_plain_block(c, key, iv, prev_block):
-    plain_block = []
+def extract_plain_block(current_encrypted_block, previous_encrypted_block, brute_force_block, key, iv):
     results = [bytearray] * 8
     for i in range(8):
-        while oracle(c, key, iv) is not True:
-            c = increment_ith_byte(7-i, c)
-        # extracted the ith byte from the plain text
-        results[i] = extract_plain_byte(i+1, prev_block[7-i], c[7-i])
-        plain_block.append(results[i])
+        c = brute_force_block + current_encrypted_block
+        for k in range(256):
+            if oracle(c, key, iv) is True:
+                break
+            brute_force_block = increment_ith_byte(7-i, brute_force_block)
+            c = brute_force_block + current_encrypted_block
+        # extract the ith byte from the plain text
+        results[i] = extract_plain_byte(i+1, previous_encrypted_block[7-i], brute_force_block[7-i])
+        # end of the attack, already found all the 8 bytes in the block
+        if i == 7:
+            break
         for j in range(i + 1):
-            last_byte_to_modify = xor(i + 2, prev_block[7 - j], int.from_bytes(results[j], byteorder='big'))
-            byte_array = bytearray(c)
+            last_byte_to_modify = xor(i + 2, previous_encrypted_block[7 - j], int.from_bytes(results[j], byteorder='big'))
+            byte_array = bytearray(brute_force_block)
             byte_array[7 - j] = int.from_bytes(last_byte_to_modify, byteorder='big')
-            c = bytes(byte_array)
-    plain_block.reverse()
-    return plain_block
+            brute_force_block = bytes(byte_array)
+    return results
 
 
 def extract_plain_text(cipher_text, key, iv):
     plain_text = []
-    prev_block = iv
-    for i in range(int(len(cipher_text) / 8)):
-        c = iv + cipher_text[i*8:(i+1)*8]
-        plain_text.extend(extract_plain_block(c, key, iv, prev_block))
-        prev_block = cipher_text[i*8:(i+1)*8]
+    block_num = (int(len(cipher_text) / 8))
+    for i in range(block_num, 0, -1):
+        current_encrypted_block = cipher_text[(i-1)*8:i*8]
+        if i == 1:
+            previous_encrypted_block = iv
+        else:
+            previous_encrypted_block = cipher_text[(i-2)*8:(i-1)*8]
+        brute_force_block = b'\x00'*8
+        plain_block = extract_plain_block(current_encrypted_block, previous_encrypted_block, brute_force_block, key, iv)
+        plain_text.extend(plain_block)
     return convert_to_text(plain_text)
 
 
@@ -75,9 +84,13 @@ def main():
         print("Please provide exactly three inputs")
     else:
         cipher_text = bytes.fromhex(sys.argv[1])
-        key = bytes.fromhex(sys.argv[2])
+        try:
+            key = bytes.fromhex(sys.argv[2])
+        except ValueError:
+            key = sys.argv[2].encode()
         iv = bytes.fromhex(sys.argv[3])
         decrypted_bytes = extract_plain_text(cipher_text, key, iv)
+        decrypted_bytes.reverse()
         num_of_bytes_used_for_padding = ord(decrypted_bytes[-1])
         decrypted_text = decrypted_bytes[0:len(decrypted_bytes) - num_of_bytes_used_for_padding]
         print(''.join(decrypted_text))
@@ -85,3 +98,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
